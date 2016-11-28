4e7f825dabe2de005e5359f6e8bd3788ceb44886
Niranjan <Niranjan@192.168.0.8>
2016-11-27 22:56:00 -0600

    Query Function updated

--- GoChainCode/src/moneygram/mgichaincode.go
+++ GoChainCode/src/moneygram/mgichaincode.go
@@ -7,6 +7,8 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+var logger = shim.NewLogger("CLDChaincode")
+
 //==============================================================================================================================
 //	 Structure Definitions
 //==============================================================================================================================
@@ -80,6 +82,22 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("Query is running " + function)
 	
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+	
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
@@ -107,7 +125,7 @@ func (t *SimpleChaincode) create_event(stub shim.ChaincodeStubInterface, args []
 		return nil, errors.New("Error converting TransactionEvent record") 
 	}
 
-	err = stub.PutState("temp", bytes)
+	err = stub.PutState("tr1", bytes)
 	if err != nil { 
 		fmt.Printf("SAVE_CHANGES: Error storing TransactionEvent record: %s", err); 
 		return nil, errors.New("Error storing TransactionEvent record") 
